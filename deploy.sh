ea74f124c8e8247c2dbd23d89fbfddacd10673c8
Dan DeGreef <dan.degreef@offers.com>
2014-12-05 19:22:59 -0600

    refactoring deploy

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ git checkout master
 echo Updating to latest version...
 git pull origin master
 
-echo Cleaning up used GPIO pins...
+echo Cleaning up GPIO pins that openHAB currently uses...
 pins=( 5 13 17 )
 for pin in "${pins[@]}"
 do
-  printf "Cleaning pin ${pin}..."
+  echo "Cleaning pin ${pin}..."
   sudo sh -c 'echo ${pin} > /sys/class/gpio/unexport'
 done
 
