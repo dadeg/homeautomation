0bf4e2275aee769edd2f47f5b4af7f50afebf121
Dan DeGreef <daniel_degreef@condenast.com>
2017-01-11 21:50:11 -0600

    add rc switch script

--- configurations/scripts/RCSwitch.sh
+++ configurations/scripts/RCSwitch.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 #run the codesend command 10 times to ensure the switch doesn't miss the command
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
-sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend argv[1] argv[2] argv[3]
+sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
+sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
+sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
