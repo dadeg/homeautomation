888a5880081dc2df776cdb7a48c7e8b9cde511e1
Dan DeGreef <daniel_degreef@condenast.com>
2017-01-11 21:53:26 -0600

    update rules with new script for RC

--- configurations/scripts/RCSwitch.sh
+++ configurations/scripts/RCSwitch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#run the codesend command 10 times to ensure the switch doesn't miss the command
+#run the codesend command 3 times to ensure the switch doesn't miss the command
 sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
 sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
 sudo /opt/433mhz-transceiver/433Utils/RPi_utils/codesend $1 $2 $3
